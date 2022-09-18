03c2e5575c5afba029907e439aeecaaac081b75a
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2022-09-18 19:02:19 +0530

    input-variables.tf in Terraform-Variables.txt

---
+++ 03-Terraform-Input-Ouput-Variables-Datasources/ec2-demo.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_version = "~> 1.2"
+  required_providers {
+    aws = {
+      version = "4.0"
+      source = "Hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+  profile = "default"
+}
+
+resource "aws_instance" "TerraformEC2Demo1" {
+  instance_type = var.instance_type
+  key_name = var.instance_key_pair
+  tags = {
+    Name = "TerraformEC2Demo1"
+  }
+  availability_zone = var.availability_zone
+  associate_public_ip_address = var.associate_public_ip_address
+  user_data = file("${path.module}/user-data-script.sh")
+}
---
+++ 03-Terraform-Input-Ouput-Variables-Datasources/input-variables.tf
@@ -0,0 +1,33 @@
+/*
+This file is for managing input variables which will be used to provision EC2 instance
+*/
+
+variable "aws_region" {
+  type = string
+  description = "Specify the region in which AWS resources needs to be created"
+  default = "us-east-1"
+}
+
+variable "instance_type" {
+  type = string
+  description = "Specify EC2 instance type"
+  default = "t2.micro"
+}
+
+variable "instance_key_pair" {
+  description = "EC2 keypair that needs to be associated with EC2 instance for SSH"
+  type = string
+  default = "terraform-key.pem"
+}
+
+variable "availability_zone" {
+  type = string
+  description = "AZ in which AWS resource needs to be created"
+  default = "us-east-1a"
+}
+
+variable "associate_public_ip_address" {
+  type = bool
+  description = "Specify if a Public IP needs to be associated with EC2 instance"
+  default = true
+}
