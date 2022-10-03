551e361f492d6c8f02403d1e704f17377b9af52a
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2022-10-03 20:54:19 +0530

    Learn about variables in terraform

---
+++ 04-Terraform-Variables/04a-Terraform-Input-variables-basics/variables.tf
@@ -0,0 +1,35 @@
+variable "instance_type" {
+  default     = "t2.micro"
+  description = "EC2 instance type"
+  type        = string
+}
+
+variable "aws_region" {
+  default     = "us-east-1"
+  description = "AWS region where resources needs to be provisioned"
+  type        = string
+}
+
+variable "az" {
+  default     = "us-east-1a"
+  description = "Availability zone where resource needs to be provisioned"
+  type        = string
+}
+
+variable "resource_count" {
+  default     = 3
+  description = "No. of instances to be provisioned"
+  type        = number
+}
+
+variable "ami_id" {
+  default     = "ami-026b57f3c383c2eec"
+  type        = string
+  description = "AMI ID of the EC2 instance"
+}
+
+variable "key_name" {
+  default     = "terraform-key"
+  type        = string
+  description = "Key pair which is required to SSH into instances"
+}
---
+++ 04-Terraform-Variables/README.md
@@ -0,0 +1,12 @@
+# Terraform variables
+
+There are three types of Terraform variables.
+- Input variables.
+- Output values.
+- Local values.
+
+## 1. Input variables
+
+Input variables serve as parameters for a Terraform module, allowing aspects of the module to be customized without altering
+the modules own source code, and allowing modules to be shared between different configs.
+
