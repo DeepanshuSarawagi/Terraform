cc7b1bf235e87271022ed35f5fbe83d9849396e6
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2022-11-07 19:52:53 +0530

    Terraform module implementation
    Signed-off-by: Deepanshu Sarawagi <deepanshusarawagi@gmail.com>

---
+++ 12-Terraform-Modules/12a-Terraform-Child-Modules/ec2-terraform-module.tf
@@ -0,0 +1,19 @@
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+  for_each = toset(["one", "two", "three"])
+  name = "instance-${each.key}"
+
+  ami                    = data.aws_ami.amzlinux2.id
+  instance_type          = "t2.micro"
+  key_name               = "terraform-key-win"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-031bca5287cdcbde7"]
+  subnet_id              = "subnet-0c56e06f7a2325f0f"
+  user_data              = file("${path.module}/apache-install.sh")
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
---
+++ 12-Terraform-Modules/12a-Terraform-Child-Modules/variables.tf
@@ -0,0 +1,5 @@
+variable "aws_region" {
+  default     = "us-east-1"
+  description = "AWS region where resources needs to be provisioned"
+  type        = string
+}
