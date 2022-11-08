a4ac769b5f95fe9be645cf82a2c4159816d16414
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2022-11-08 17:40:23 +0530

    S3 bucket creation using resources
    Signed-off-by: Deepanshu Sarawagi <deepanshusarawagi@gmail.com>

--- 12-Terraform-Modules/12b-Terraform-Modules-Build/main.tf
+++ 12-Terraform-Modules/12b-Terraform-Modules-Build/main.tf
@@ -13,17 +13,20 @@ resource "aws_s3_bucket_policy" "s3_bucket_policy" {
   bucket = aws_s3_bucket.s3_bucket.id
   policy = <<EOF
                 {
-                  "Version": "2012-10-17",
-                  "Id": "Policy1667898386224",
-                  "Statement": [
-                      {
-                          "Sid": "Stmt1667898378183",
-                          "Effect": "Allow",
-                          "Principal": "*",
-                          "Action": "s3:GetObject",
-                          "Resource": "arn:aws:s3:::${var.bucket_name}/*"
-                      }
-                  ]
+                "Id": "Policy1667905055756",
+                "Version": "2012-10-17",
+                "Statement": [
+                  {
+                    "Sid": "Stmt1667905043931",
+                    "Action": [
+                      "s3:GetObject",
+                      "s3:PutObject"
+                    ],
+                    "Effect": "Allow",
+                    "Resource": "arn:aws:s3:::${var.bucket_name}/*",
+                    "Principal": "*"
+                  }
+                ]
               }
           EOF
 }
@@ -33,4 +36,15 @@ resource "aws_s3_bucket_website_configuration" "aws_s3_bucket_website" {
   index_document {
     suffix = "product-landing-page.html"
   }
+  depends_on = [aws_s3_object.aws_s3_bucket_object]
+}
+
+resource "aws_s3_object" "aws_s3_bucket_object" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  key    = "product-landing-page.html"
+  source = "product-landing-page.html"
+  content_type = "text/html"
+  acl = "public-read"
+  etag = filemd5("${path.module}/product-landing-page.html")
+  depends_on = [aws_s3_bucket.s3_bucket, aws_s3_bucket_acl.s3_bucket_acl, aws_s3_bucket_policy.s3_bucket_policy]
 }
