38307558a84676eb3726e1b155c0ece7db1f372d
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2024-02-07 19:10:56 +0530

    bastion ec2 instance definition

---
+++ 07-AWS-EC2-BastionHosts/data.tf
@@ -0,0 +1,24 @@
+data "aws_ami" "amzlinux2" {
+  owners = ["amazon"]
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-kernel-6.1-x86_64"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
---
+++ 07-AWS-EC2-BastionHosts/ec2.tf
@@ -0,0 +1,16 @@
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "5.6.0"
+  name = "${local.name}-bastion-host"
+
+  instance_type          = var.instance_type
+  key_name               = var.instance_keypair
+  monitoring             = false
+  ami = data.aws_ami.amzlinux2.id
+  vpc_security_group_ids = [module.public_bastion_sg.security_group_id]
+  subnet_id              = module.vpc.public_subnets[0]
+
+  tags = merge({
+    Terraform   = "true"
+  },local.common_tags)
+}
