e5346bcd3ae64ccb2d217e77146aa60c5824b6a8
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2024-02-07 12:13:58 +0530

    create local variables

--- 06-Terraform-AWS-VPC/ec2-variables.tf
+++ 06-Terraform-AWS-VPC/ec2-variables.tf
@@ -56,7 +56,7 @@ variable "environment" {
   default = "dev"
 }
 # Business Division
-variable "business_divsion" {
+variable "business_division" {
   description = "Business Division in the large organization this Infrastructure belongs"
   type = string
   default = "SAP"
