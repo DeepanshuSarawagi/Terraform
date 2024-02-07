20d2c89ffa35a4c5655f43323d3e43e994d02a7d
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2024-02-07 18:48:49 +0530

    vpc and bastion host section

---
+++ 07-AWS-EC2-BastionHosts/locals.tf
@@ -0,0 +1,9 @@
+locals {
+  owners = var.business_division
+  environment = var.environment
+  name = "${local.owners}-${local.environment}"
+  common_tags = {
+    owners = local.owners
+    environment = local.environment
+  }
+}
