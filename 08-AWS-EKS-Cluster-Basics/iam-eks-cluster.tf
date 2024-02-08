66e6a19b74690bb727464e4969b253f7c872a3ef
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2024-02-08 18:58:54 +0530

    EKS cluster IAM nodegroup role

--- 08-AWS-EKS-Cluster-Basics/iam-eks-cluster.tf
+++ 08-AWS-EKS-Cluster-Basics/iam-eks-cluster.tf
@@ -18,11 +18,20 @@ resource "aws_iam_role" "eks_master_role" {
 POLICY
 }
 
-# Associate IAM Policy to IAM Role
+/*
+This policy provides Kubernetes the permissions it requires to manage resources on your behalf.
+Kubernetes requires Ec2:CreateTags permissions to place identifying information on EC2 resources including but not
+limited to Instances, Security Groups, and Elastic Network Interfaces.
+*/
+
 resource "aws_iam_role_policy_attachment" "eks-AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = aws_iam_role.eks_master_role.name
 }
+
+/*
+Policy used by VPC Resource Controller to manage ENI and IPs for worker nodes.
+*/
 
 resource "aws_iam_role_policy_attachment" "eks-AmazonEKSVPCResourceController" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
