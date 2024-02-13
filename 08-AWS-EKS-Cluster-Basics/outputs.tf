7085d2c13fbaa59362935e8444f45923b8c97571
Deepanshu Sarawagi <48376209+DeepanshuSarawagi@users.noreply.github.com>
2024-02-13 14:23:55 +0530

    EKS outpits

--- 08-AWS-EKS-Cluster-Basics/outputs.tf
+++ 08-AWS-EKS-Cluster-Basics/outputs.tf
@@ -45,3 +45,71 @@ output "ec2_bastion_public_instance_ids" {
 output "ec2_bastion_public_ip" {
   value = aws_eip.bastion_eip.public_ip
 }
+
+# EKS specific outputs
+
+output "cluster_id" {
+  description = "The name/id of EKS cluster"
+  value = aws_eks_cluster.eks_cluster.id
+}
+
+output "cluster_arn" {
+  description = "The ARN of EKS cluster"
+  value = aws_eks_cluster.eks_cluster.arn
+}
+
+output "cluster_certificate_authority_data" {
+  description = "Nested attribute containing certificate-authority-data for your cluster. This is the base64 encoded certificate data required to communicate with your cluster."
+  value       = aws_eks_cluster.eks_cluster.certificate_authority[0].data
+}
+
+output "cluster_endpoint" {
+  description = "The endpoint for your EKS Kubernetes API."
+  value       = aws_eks_cluster.eks_cluster.endpoint
+}
+
+output "cluster_version" {
+  description = "The Kubernetes server version for the EKS cluster."
+  value       = aws_eks_cluster.eks_cluster.version
+}
+
+output "cluster_iam_role_name" {
+  description = "IAM role name of the EKS cluster."
+  value       = aws_iam_role.eks_master_role.name 
+}
+
+output "cluster_iam_role_arn" {
+  description = "IAM role ARN of the EKS cluster."
+  value       = aws_iam_role.eks_master_role.arn
+}
+
+output "cluster_oidc_issuer_url" {
+  description = "The URL on the EKS cluster OIDC Issuer"
+  value       = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
+}
+
+output "cluster_primary_security_group_id" {
+  description = "The cluster primary security group ID created by the EKS cluster on 1.14 or later. Referred to as 'Cluster security group' in the EKS console."
+  value       = aws_eks_cluster.eks_cluster.vpc_config[0].cluster_security_group_id
+}
+
+# EKS Node Group Outputs - Public
+output "node_group_public_id" {
+  description = "Public Node Group ID"
+  value       = aws_eks_node_group.eks_public_ng.id
+}
+
+output "node_group_public_arn" {
+  description = "Public Node Group ARN"
+  value       = aws_eks_node_group.eks_public_ng.arn
+}
+
+output "node_group_public_status" {
+  description = "Public Node Group status"
+  value       = aws_eks_node_group.eks_public_ng.status 
+}
+
+output "node_group_public_version" {
+  description = "Public Node Group Kubernetes Version"
+  value       = aws_eks_node_group.eks_public_ng.version
+}
