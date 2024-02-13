da1a928e6161c59484caaef97fb7fe1fba501f5e
Deepanshu Sarawagi <48376209+DeepanshuSarawagi@users.noreply.github.com>
2024-02-13 12:52:03 +0530

    Node group configuration

--- 08-AWS-EKS-Cluster-Basics/eks-ng-public.tf
+++ 08-AWS-EKS-Cluster-Basics/eks-ng-public.tf
@@ -2,5 +2,16 @@
 
 resource "aws_eks_node_group" "eks_public_ng"  {
 
-  cluster_name = 
+  cluster_name = aws_eks_cluster.eks_cluster.name
+  node_group_name = "${local.name}-eks-ng-public"
+  node_role_arn = aws_iam_role.eks_nodegroup_role.arn
+  subnet_ids = module.vpc.public_subnets
+  version = var.cluster_version
+
+  scaling_config = {
+    desired_size = 1
+    min_size = 1
+    max_size = 2
+  }
+
 }
