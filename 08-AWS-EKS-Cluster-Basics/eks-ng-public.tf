c00f8b34e633f757d8ef41fefc5f3473d8da3e7f
Deepanshu Sarawagi <48376209+DeepanshuSarawagi@users.noreply.github.com>
2024-02-13 13:00:42 +0530

    remote access config for nodegroup

--- 08-AWS-EKS-Cluster-Basics/eks-ng-public.tf
+++ 08-AWS-EKS-Cluster-Basics/eks-ng-public.tf
@@ -7,6 +7,18 @@ resource "aws_eks_node_group" "eks_public_ng"  {
   node_role_arn = aws_iam_role.eks_nodegroup_role.arn
   subnet_ids = module.vpc.public_subnets
   version = var.cluster_version
+  ami_type = "AL2_x86_64"
+  capacity_type = "ON_DEMAND"
+  disk_size = 30
+  instance_types = ["t3.medium"]
+
+  # Access block for Node group
+
+  remote_access = {
+    ec2_ssh_key = var.instance_keypair
+    
+
+  }
 
   scaling_config = {
     desired_size = 1
