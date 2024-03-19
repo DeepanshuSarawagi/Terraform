d2dd9c983211cacd8bffb6acde850373de6160c2
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2024-03-19 09:12:25 +0530

    Config terraform kubernetes provider

--- 11-Terraform-Kubernetes-Provider/data.tf
+++ 11-Terraform-Kubernetes-Provider/data.tf
@@ -4,3 +4,11 @@ data "terraform_remote_state" "eks" {
     path = "../08-AWS-EKS-Cluster-Basics/terraform.tfstate"
   }
 }
+
+data "aws_eks_cluster" "cluster" {
+  name = data.terraform_remote_state.eks.outputs.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster_auth" {
+  name = data.terraform_remote_state.eks.outputs.cluster_id
+}
--- 11-Terraform-Kubernetes-Provider/versions.tf
+++ 11-Terraform-Kubernetes-Provider/versions.tf
@@ -18,5 +18,7 @@ provider "aws" {
 }
 
 provider "kubernetes" {
-
+  host = data.terraform_remote_state.eks.outputs.cluster_endpoint
+  client_certificate = base64decode(data.terraform_remote_state.eks.outputs.cluster_certificate_authority_data)
+  token = data.aws_eks_cluster_auth.cluster_auth.token
 }
